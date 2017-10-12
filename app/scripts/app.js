da13bc1ac7f3a61e1ec20c3ccfab0c9237c3cdb9
jean luc <hrluc13@gmail.com>
2017-10-12 17:43:13 +0200

    Version initiall

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontEndApp
+ * @description
+ * # frontEndApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontEndApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
